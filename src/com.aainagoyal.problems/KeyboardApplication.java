42025ef5636c176320e3b828d09fc0f9d547433a
Aaina Goyal <aaina.goyal4@gmail.com>
2019-05-02 01:53:05 -0400

    Add Keyboard Application File

---
+++ src/com.aainagoyal.problems/KeyboardApplication.java
@@ -0,0 +1,30 @@
+package com.aainagoyal.problems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by aainagoyal on 5/2/19.
+ */
+public class KeyboardApplication {
+    public String[] findWords(String[] words) {
+        List<Character> listOfCharacter1 = new ArrayList<Character>();
+        List<Character> listOfCharacter2 = new ArrayList<Character>();
+        List<Character> listOfCharacter3 = new ArrayList<Character>();
+        String[] solutionString;
+        int count = 0;
+        Collections.addAll(listOfCharacter1, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p');
+        Collections.addAll(listOfCharacter2, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l');
+        Collections.addAll(listOfCharacter2, 'z', 'x', 'c', 'v', 'b', 'n', 'm');
+        for (int i = 0; i < words.length; i++)
+            {
+                
+            }
+
+
+
+
+    }
+}
+
